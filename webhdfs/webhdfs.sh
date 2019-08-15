2b2c3eb7881be14f894e05f24ed19cfe9bed0412
sidkalra <sidhartha.kalra@gmail.com>
2019-08-15 09:28:42 +0100

    fixing user

--- webhdfs/webhdfs.sh
+++ webhdfs/webhdfs.sh
@@ -2,7 +2,7 @@ curl -i -L -X PUT -H "Content-Type: text/plain" -d "Welcome to the code challeng
 curl -i -X POST -L -H "Content-Type: text/plain" -d "we wish you all the best" "http://localhost:9870/webhdfs/v1/user/hdfs/iovio.dat?op=APPEND&user.name=hadoop" >> log.txt
 curl -i -L "http://localhost:9870/webhdfs/v1/user/hdfs/iovio.dat?op=OPEN&user.name=hadoop" >> log.txt
 
-if [ "$(curl -L 'http://localhost:9870/webhdfs/v1/user/hdfs/iovio.dat?op=OPEN&user.name=ec2-user')" == "Welcome to the code challenge we wish you all the best" ]
+if [ "$(curl -L 'http://localhost:9870/webhdfs/v1/user/hdfs/iovio.dat?op=OPEN&user.name=hadoop')" == "Welcome to the code challenge we wish you all the best" ]
 then
 echo Passed
 else
